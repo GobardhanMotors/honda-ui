c5824af6d628c1df0a01b114daa524d9575693c1
Shubham Chaturvedi <shubham1838@gmail.com>
2017-12-05 03:50:36 +0530

    final
    final

--- test/test.config.js
+++ test/test.config.js
@@ -116,7 +116,57 @@ var timeStamp = "bust=" + (new Date()).getTime(),
   });
 }());
 
+----------------------------------------------------Route.js---------------------------------------------
+(function () {
+  'use strict';
+  define(['angular', 'app'], function (angular, app) {
 
+    var appModule = angular.module('appModule', ['app'])
+      .config(['$stateProvider', '$urlRouterProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$locationProvider',
+        function ($stateProvider, $urlRouterProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $locationProvider) {
+          var route = routeResolverProvider.route;
+          appModule.register = {
+            controller: $controllerProvider.register,
+            directive: $compileProvider.directive,
+            filter: $filterProvider.register,
+            factory: $provide.factory,
+            service: $provide.service
+          };
+          $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+          });
+          $urlRouterProvider.otherwise('/');
+          $stateProvider
+          .state("app", {
+            url: "",
+            views: {
+              header: {
+                templateUrl: '/modules/view/header.html?' + timeStamp,
+                controller: 'headerController',
+                resolve: route.resolve(['headerController']),
+              },
+              container: {
+                templateUrl: '/modules/view/contact.html?' + timeStamp,
+              },
+              footer: {
+                templateUrl: '/modules/view/footer.html?' + timeStamp,
+                controller: 'footerController',
+                resolve: route.resolve(['footerController']),
+              }
+            }
+          })
+          .state('app.home', {
+            url: '/',
+            templateUrl: '/modules/view/home.html?' + timeStamp,
+            controller: 'homeController',
+            resolve: route.resolve(['homeController'])
+          });
+        }]);
+    appModule.register = appModule;
+    return appModule;
+  });
+}());
 
 
 --------------------------------------------routeResolver.js-------------------------------------------------------
